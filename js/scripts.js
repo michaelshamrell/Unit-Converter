86233d62ff505bf9da5d820f1a66f7f619e46fa7
michaelshamrell <michaelshamrell2001@gmail.com>
2022-02-08 19:23:38 -0800

    Finish a working Unit Converter

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,49 @@
 
-
-let meter = 0
-let feet = 0
-let mtf = 0
-
-
-
-
+//Meter to Feet Function
 function meterToFeetCalculator() {
   meter = Number(document.getElementById("meterToFeetInput").value);
-  let feet = 3.281
-  mtf = meter * feet
-  console.log(mtf)
-  let mtfEl = document.getElementById("mtf-el")
-  mtfEl.textContent = mtf
-  console.log(mtfEl)
-  
+  let feet = 3.281;
+  mtf = (meter * feet).toFixed(3);
+  let mtfEl = document.getElementById("mtf-el");
+  mtfEl.textContent = mtf;
+}
+//Feet to Meters Function
+function feetToMeterCalculator() {
+  feet = Number(document.getElementById("feetToMeterInput").value);
+  let meter = .305;
+  ftm = (feet * meter).toFixed(3);
+  let ftmEl = document.getElementById("ftm-el");
+  ftmEl.textContent = ftm;
+}
+//Liter to Gallon Function
+function literToGallonCalculator() {
+  liter = Number(document.getElementById("literToGallonInput").value);
+  let gallon = 0.219969;
+  ltg = (liter * gallon).toFixed(3);
+  let ltgEl = document.getElementById("ltg-el");
+  ltgEl.textContent = ltg;
+}
+//Liter to Gallon Function
+function gallonToLiterCalculator() {
+  gallon = Number(document.getElementById("gallonToLiterInput").value);
+  let liter = 3.78541;
+  gtl = (gallon * liter).toFixed(3);
+  let gtlEl = document.getElementById("gtl-el");
+  gtlEl.textContent = gtl;
+}
+//Kg to Pounds Function
+function kgToPoundCalculator() {
+  kg = Number(document.getElementById("kgToPoundInput").value);
+  let pound = 2.20462;
+  ktp = (kg * pound).toFixed(3);
+  let ktpEl = document.getElementById("ktp-el");
+  ktpEl.textContent = ktp;
+}
+//Pounds to Kg Function
+function poundToKgCalculator() {
+  pound = Number(document.getElementById("poundToKgInput").value);
+  let kg = 0.453592;
+  ptk = (pound * kg).toFixed(3);
+  let ptkEl = document.getElementById("ptk-el");
+  ptkEl.textContent = ptk;
 }
